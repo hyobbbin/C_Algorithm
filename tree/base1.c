1870b73cd5485d687d8ce440fae5f927b1bc43e9
HyoBin Lee <130888836+hyobbbin@users.noreply.github.com>
2025-05-15 23:02:04 +0900

    Update and rename base.c to base1.c

--- tree/base1.c
+++ tree/base1.c
@@ -30,7 +30,37 @@ int main(void){
 }
 
 //전위순회 1.루트 노드 방문 2. 왼쪽 서브트리 방문 3. 오른쪽 서브트리 방문
-
+void preorder(treeNode *root){
+  if(root){
+    printf("%d ", root -> key);
+    preorder(root -> leftChild);
+    preorder(root -> rightChild);
+  }
+}
 //중위순회 1. 왼쪽 서브트리 방문 2. 루트 노드 방문 3. 오른쪽 서브트리 방문
-
+void inorder(treeNode *root){
+  if(root){
+    inorder(root -> leftChild);
+    printf("%d ", root -> key);
+    inorder(root -> rightChild);
+  }
+}
 //후위순회 1. 왼쪽 서브트리 방문 2. 오른쪽 서브트리 방문 3. 루트 노드 방문
+void postorder(treeNode *root){
+  if(root){
+    postorder(root -> leftChild);
+    postorder(root -> rightChild);
+    printf("%d ", root -> key);
+  }
+}
+//레벨순회
+void level_order(treeNode *root){
+  int queue;
+  if(root == NULL) break;
+  enqueue(root);
+  while(!is_empty()){
+    printf("%d ", dequeue());
+    if((root -> leftChild) != '\n') enqueue(root -> leftChild);
+    if((root -> rightChild) != '\n') enqueue(root -> rightChild);
+  }
+}
